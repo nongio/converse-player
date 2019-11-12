574b481d0f20b134e726323dd7bbc2da5ce4a36f
robot <robot@converse-player.com>
2019-11-12 09:50:04 +0000

    CircleCI build 24 from master

---
+++ precache-manifest.0451afc69d042bd82baf207488f2b467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ba929361d2dc6b064b56988a7b88a7",
+    "url": "/converse-player/index.html"
+  },
+  {
+    "revision": "042fb70a61fda4e68457",
+    "url": "/converse-player/static/css/main.6ef5b06a.chunk.css"
+  },
+  {
+    "revision": "e9308bbd5e435d035786",
+    "url": "/converse-player/static/js/2.fb88cbfd.chunk.js"
+  },
+  {
+    "revision": "042fb70a61fda4e68457",
+    "url": "/converse-player/static/js/main.429e8f37.chunk.js"
+  },
+  {
+    "revision": "366cb198bd2b855b90fb",
+    "url": "/converse-player/static/js/runtime~main.ec38fd54.js"
+  }
+]);
